0ff2de6aa7550e04ad54e082ac4993d7ff165ce1
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2016-08-10 14:56:31 +0800

    Update make-img.sh

--- make-img.sh
+++ make-img.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
- export ARCH=arm 
-  export CROSS_COMPILE=arm-linux-gnueabihf-
+export ARCH=arm 
+export CROSS_COMPILE=arm-none-eabi-
 
 LOCALPATH=$(pwd)
 ROOTDIR=$(dirname $(dirname $(readlink -f $0)))
@@ -37,13 +37,17 @@ case ${BOARD} in
         DTB=rk3036-kylin.dtb
         ;;
     "firefly")
-        DEFCONFIG=""
+        DEFCONFIG=rockchip_linux_defconfig
         DTB=rk3288-firefly.dtb
         ;;
     "rk3036evb")
-        DEFCONFIG=""
+        DEFCONFIG=rockchip_linux_defconfig
         DTB=rk3036-evb.dtb
         ;;        
+     "fennec")
+        DEFCONFIG=rockchip_linux_defconfig
+        DTB=rk3288-fennec.dtb
+        ;;               
     *)
         echo "board '${BOARD}' not supported!"
         return
@@ -59,9 +63,12 @@ make  -j8
 scripts/mkkrnlimg arch/arm/boot/zImage kernel.img>/dev/null
 echo '  Image:  kernel.img is ready'
 scripts/resource_tool arch/arm/boot/dts/${DTB}
-echo '  Image:  resource.img (with ${DTB} is ready'
+echo " Image:  resource.img (with '${DTB}' is ready"
 
 cat arch/arm/boot/zImage arch/arm/boot/dts/${DTB} > kernel2.img.zimage
 mkimage -A arm -O linux -T kernel -C none -a 0x60800800 -d kernel2.img.zimage kernel2.img
 rm kernel2.img.zimage
 echo '  Image:  kernel2.img (with ${DTB} is ready'
+
+cp arch/arm/boot/zImage ./
+cp arch/arm/boot/dts/${DTB} ./
