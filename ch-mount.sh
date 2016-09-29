2f00dc2c88b3d90621f5f8fca5485f9f69bcd7f7
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2016-09-29 15:58:08 +0800

    Update ch-mount.sh

--- ch-mount.sh
+++ ch-mount.sh
@@ -5,7 +5,7 @@ function mnt() {
     sudo mount -t proc /proc ${2}proc
     sudo mount -t sysfs /sys ${2}sys
     sudo mount -o bind /dev ${2}dev
-
+    sudo mount --bind / ${2}host
     sudo chroot ${2}
 }
 
@@ -14,7 +14,7 @@ function umnt() {
     sudo umount ${2}proc
     sudo umount ${2}sys
     sudo umount ${2}dev
-
+    sudo umount ${2}host
 }
 
 
