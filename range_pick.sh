3f9ce47d7bb2bdf78238421edb6a8a43d57cb1d1
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2016-12-29 16:41:50 +0800

    Update range_pick.sh

--- range_pick.sh
+++ range_pick.sh
@@ -1,5 +1,12 @@
-git cherry-pick -s -x $1
+#/bin/bash
+
+git cherry-pick  -x $1
 LOG=`git log -n 1 --pretty=format:%s`
 BODY=`git log -n 1 --pretty=format:%b`
 NEWLOG="UPSTREAM: "$LOG
 git commit --amend -m "$NEWLOG" -m "$BODY"
+
+cat << EOF |  git commit -s --amend 
+:wq
+
+EOF
