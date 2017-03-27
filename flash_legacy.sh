1adf2ac5d9ff1efc8d999841a684c3e5556a2191
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2017-03-27 14:06:46 +0800

    Update flash_legacy.sh

--- flash_legacy.sh
+++ flash_legacy.sh
@@ -2,11 +2,13 @@ sudo upgrade_tool ul loader.bin
 sudo upgrade_tool di -p parameter
 sudo upgrade_tool di -k kernel.img
 
-# rk3288
+# rk3288需要烧这个
 sudo upgrade_tool di -b boot_no_package_kernel.img
 
+# rk3399需要烧这几个
 sudo upgrade_tool di uboot uboot.img || true
 sudo upgrade_tool di trust trust.img || true
+
 sudo upgrade_tool di resource resource.img
 sudo upgrade_tool di linuxroot root.img
 sudo upgrade_tool rd
