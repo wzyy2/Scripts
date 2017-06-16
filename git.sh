577ed56505853dc52e0b7e8e9e8644f0a222e724
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2017-06-16 16:47:23 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,1 +1,4 @@
 git branch --set-upstream-to=origin/morty morty
+
+make ARCH=arm savedefconfig && mv defconfig arch/arm/configs/rockchip_linux_defconfig
+make ARCH=arm64 savedefconfig && mv defconfig arch/arm64/configs/rockchip_linux_defconfig
