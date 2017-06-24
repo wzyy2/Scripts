b14780ac4f04c2ca8cfd0cddb5b78659a3ce60d2
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2017-06-24 16:09:56 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,3 +2,12 @@ git branch --set-upstream-to=origin/morty morty
 
 make ARCH=arm savedefconfig && mv defconfig arch/arm/configs/rockchip_linux_defconfig
 make ARCH=arm64 savedefconfig && mv defconfig arch/arm64/configs/rockchip_linux_defconfig
+
+/workbench/debian-sdk/kernel/scripts/Lindent  *.c
+
+
+astyle *.c  --style=knf --indent=tab
+
+./scripts/checkpatch.pl -f outging/*
+./scripts/get_maintainer.pl arch/arm/mach-rockchip -f
+git send-email outgoing/* --to ***@gmail.com --cc ***@rock-chips.com
