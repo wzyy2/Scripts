708837432f773a45a33a19bee8bb20db3b31b030
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2017-08-22 10:37:12 +0800

    Update vncserver.sh

--- vncserver.sh
+++ vncserver.sh
@@ -5,3 +5,13 @@ https://www.digitalocean.com/community/tutorials/how-to-set-up-vnc-server-on-deb
 tightvncserver
 apt-get install  xfonts-base
 export DISPLAY=:1.0
+
+
+#tigervnviewer
+#tigervnc-common
+
+while true
+do
+	vncviewer 192.168.31.236:1 DesktopSize=1024x768 QualityLevel=5 passwd=/root/.vnc/passwd
+  sleep 20
+done
